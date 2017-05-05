26e00f73c3cc8342e90165f3febeb9bac08a2168
Aaron Lord <aaron@lorddesign.net>
2017-05-04 21:25:09 -0700

    Add file reader with line-by-line callback

---
+++ test/TwoCities.Tests/FileReader.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoCities.Tests
+{
+    #pragma warning disable S3881 // "IDisposable" is already implemented correctly
+    public class FileReader : IDisposable
+    {
+        private readonly StreamReader _fileStream;
+
+        public Action<string> OnLineRead { get; set; } = s => { };
+
+        public FileReader(string path)
+        {
+            _fileStream = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read));
+        }
+
+
+        public async Task Stream()
+        {
+            string line;
+            while ((line = _fileStream.ReadLine()) != null)
+            { 
+                await Task.Delay(20);
+                foreach (var evt in OnLineRead.GetInvocationList())
+                {
+                    await Task.Run(() =>
+                    {
+                        try
+                        {
+                            evt.DynamicInvoke(line);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+
+                    });
+                }
+
+                Console.WriteLine("Batch complete...");
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _fileStream.Dispose();
+            }
+        }
+    }
+}
---
+++ test/TwoCities.Tests/FileStreamLineTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TwoCities.Tests
+{
+    public class FileStreamLineTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public FileStreamLineTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        /// <summary>
+        /// Tests that the reader splits by lines and performs a function at each interval.
+        /// </summary>
+        /// <remarks>Basically what it's doing is reading the file in small batches. When it encounters a line
+        /// break, it makes a note of it, puts the extra text in a "buffer" (not to be confused with an actual stream
+        /// buffer) </remarks>
+        /// <returns></returns>
+        [Fact]
+        public async Task TestFileStreamSegmented()
+        {
+            var list = new List<string>();
+            using (var fs = new FileReader(AppContext.BaseDirectory + @"\lipsum.log"))
+            {
+                // put stuff between line breaks right here.
+                fs.OnLineRead += s =>
+                {
+                    var stringToAdd = s.Trim();
+                    list.Add(stringToAdd);
+                    _output.WriteLine($"Added {stringToAdd}");
+                };
+
+                await fs.Stream();
+            }
+
+            _output.WriteLine($"List with {list.Count} entries.");
+            Assert.Equal("malesuada.", list.Last());
+            Assert.Equal(19, list.Count);
+        }
+    }
+}
