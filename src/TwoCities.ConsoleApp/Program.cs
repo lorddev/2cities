560336bd83520c8a6a16f6443b82306872986c10
Aaron Lord <aaron@lorddesign.net>
2017-05-04 17:51:42 -0700

    Scaffolding

---
+++ src/TwoCities.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoCities.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
